b8cee4316edc858872ed5297f82e1247a875ca65
nkt <bm.hcmc.duccan@modetour.com>
2023-05-19 16:46:19 +0700

    a

--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -9,6 +9,9 @@ import coccinella from "../../assets/images/coccinella.png";
 import { LANGUAGE } from "../../utils";
 import { changeLanguageApp } from "../../store/actions";
 import { FormattedMessage } from "react-intl";
+import { path } from "../../utils";
+import Home from "../../routes/Home";
+import { Route } from "react-router-dom";
 class HomeHeader extends Component {
   changeLanguage = (language) => {
     this.props.changeLanguageApp(language);
